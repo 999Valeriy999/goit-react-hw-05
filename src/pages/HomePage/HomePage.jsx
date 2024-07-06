313cba21ab0cc25404f1dcb41b65a6513bc3539d
Valeriy Borodenko <valeriyy999@gmail.com>
2024-07-07 00:40:31 +0300

    add

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const DataFetcher = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/account/null/favorite/tv?language=en-US&page=1&sort_by=created_at.asc')
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);  
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Fetched Data</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DataFetcher;
