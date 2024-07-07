e0decceb85ce837bfad4e74a315720e4241c8a07
Valeriy Borodenko <valeriyy999@gmail.com>
2024-07-08 00:38:43 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import MoviesPage from './pages/MoviesPage/MoviesPage';
 
-
 function App() {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const handleSearchSubmit = (newQuery) => {
+    setQuery(newQuery);
+    setImages([]);
+    setPage(1);
+  };
+
   return (
     <BrowserRouter>
-      <>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <NavLink to="/">HomePage</NavLink>
-              </li>
-              <li>
-                <NavLink to="/movies">MoviesPage</NavLink>
-              </li>
-            </ul>
-          </nav>
-
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/movies" element={<MoviesPage />} />
-          </Routes>
-        </div>
-      </>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">HomePage</NavLink>
+            </li>
+            <li>
+              <NavLink to="/movies">MoviesPage</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage onSubmit={handleSearchSubmit} />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
 
 export default App;
-
-
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import toast, { Toaster } from "react-hot-toast";
 import css from "./MoviesPage.module.css";
 import { AiFillWarning } from "react-icons/ai";
 import { Formik, Form, Field } from "formik";
 
-const SearchBar = ({ onSubmit }) => {
+const MoviesPage = ({ onSubmit }) => {
     const handleSubmit = (values, actions) => {
         if (values.query.trim() === "") {
             toast("Please, enter your query", {
@@ -18,24 +19,28 @@ const SearchBar = ({ onSubmit }) => {
     };
 
     return (
-        <header className={css.searchBar}>
-            <Toaster position="top-right" />
-            <Formik initialValues={{ query: "" }} onSubmit={handleSubmit}>
-                <Form className={css.form}>
-                    <Field
-                        className={css.input}
-                        type="text"
-                        name="query"
-                        autoComplete="off"
-                        autoFocus
-                        placeholder="Search images and photos"
-                    ></Field>
-                    <button className={css.btn} type="submit">
-                        Search
-                    </button>
-                </Form>
-            </Formik>
-        </header>
+        <div>
+            <header className={css.searchBar}>
+                <Toaster position="top-right" />
+                <Formik initialValues={{ query: "" }} onSubmit={handleSubmit}>
+                    <Form className={css.form}>
+                        <Field
+                            className={css.input}
+                            type="text"
+                            name="query"
+                            autoComplete="off"
+                            autoFocus
+                            placeholder="Search images and photos"
+                        />
+                        <button className={css.btn} type="submit">
+                            Search
+                        </button>
+                    </Form>
+                </Formik>
+            </header>
+            {/* Add your MoviesPage content here */}
+        </div>
     );
 };
+
 export default MoviesPage;
