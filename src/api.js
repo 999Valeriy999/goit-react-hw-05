a9ed2ddb41b45480c53c13b6729c6e0829a0a990
Valeriy Borodenko <valeriyy999@gmail.com>
2024-07-14 00:15:01 +0300

    home page

--- src/api.js
+++ src/api.js
@@ -1,19 +1,14 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://www.themoviedb.org/";
-const API_KEY = "28914ce15cab0b4f8d47d4bcf46462e9";
-const AUTH_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODkxNGNlMTVjYWIwYjRmOGQ0N2Q0YmNmNDY0NjJlOSIsIm5iZiI6MTcyMDcyODUxMy44ODcyOTksInN1YiI6IjY2ODVhMjBhNjNkMGI1ZDdmYTFhNDc3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rFu2IKeY4pJGKzWA4TiC_A4EHz3SlbkO2qkDWVDKCmM ";
+axios.defaults.params =
+ {api_key: "28914ce15cab0b4f8d47d4bcf46462e9",};
 
-export const fetchFilm = async (searchQuery, currentPage) => {
+export const fetchFilm = async (searchQuery) => {
   try {
     const response = await axios.get("/search/movie", {
       params: {
-        api_key: API_KEY,
         query: searchQuery,
-        page: currentPage,
-      },
-      headers: {
-        Authorization: `Bearer ${AUTH_TOKEN}`,
       },
     });
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,21 @@
 
-import React from 'react';
-import { Link } from 'react-router-dom'; // Импортируем компонент Link из React Router
-import { API_KEY } from './api'; // Импортируем API_KEY из модуля './api'
+import { useEffect } from "react";
+import { fetchFilm } from "../../api";
 
 const HomePage = () => {
-  const serviceUrl = `https://www.themoviedb.org/?api_key=${API_KEY}`;
+useEffect(() => {
+  const asyncWrapper = async () => {
+    const data = await fetchFilm("avatar");
+    console.log(data);
+  };
+
+ asyncWrapper(); }, []);
 
   return (
     <div>
       <h1>Home page</h1>
-      
-      <Link to={serviceUrl} target="_blank" rel="noopener noreferrer"></Link> 
     </div>
   );
-}
+};
 
 export default HomePage;
