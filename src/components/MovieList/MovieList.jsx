371e5739dedf465c284ed6e6548e9394f4a73481
Valeriy Borodenko <valeriyy999@gmail.com>
2024-07-16 01:10:33 +0300

    add

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,22 +1,21 @@
-import { Link, useLocation } from "react-router-dom";
-const MovieList = ({ movies }) => {
-  const location = useLocation();
+import React from 'react';
+import TrendingItem from '../TrendingItem/TrendingItem'; // Предполагается, что у вас есть компонент TrendingItem для отображения каждого элемента списка
 
+const MovieList = ({ title, movies, isLoading }) => {
   return (
-    <ul>
-      {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link
-            to={{
-              pathname: `/movies/${movie.id.toString()}`,
-              state: { from: location.pathname },
-            }}
-          >
-            {movie.title}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      {title && <h2>{title}</h2>}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {Array.isArray(movies) && movies.map((movie) => (
+            <TrendingItem key={movie.id} id={movie.id} name={movie.title} />
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
+
 export default MovieList;
---
+++ src/components/TrendingItem/TrendingItem.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { useLocation } from "react-router-dom";
+
+const TrendingItem = ({ id, name }) => {
+  const { pathname, search } = useLocation();
+
+  return (
+    <li>
+      <S.ListLink
+        state={{
+          pathname,
+          search,
+        }}
+        to={`/movies/${id}`}
+      >
+        {name}
+      </S.ListLink>
+    </li>
+  );
+};
+
+export default TrendingItem;
