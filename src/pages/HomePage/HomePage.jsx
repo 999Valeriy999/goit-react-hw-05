41dcd9e9ed51ef4cbe82b26a44688ef0d3ce568d
Valeriy Borodenko <valeriyy999@gmail.com>
2024-07-18 22:18:24 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,49 +1,58 @@
-
 import { useEffect, useState } from "react";
 import { fetchTopRatedFilms } from "../../api";
 import MovieList from '../../components/MovieList/MovieList';
 
-
 const HomePage = () => {
-const [movies, setMovies] = useState ([]);
-const [isLoading, setIsLoading] = useState(false);
-const [moviesList, setMoviesList] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null); // Состояние для хранения ошибки
 
   const handleMoviesUpdate = (movies) => {
     setMoviesList(movies);
   };
-useEffect(() => {
-  const asyncWrapper = async () => {
-    try {
-      setIsLoading(true);
-    const data = await fetchTopRatedFilms ();
-    setMovies(data);
-    } catch (error) {
-    console.log(error);
-  } finally {
-    setIsLoading(false);
-  }
-  };
 
- asyncWrapper(); }, []);
- 
+  useEffect(() => {
+    const asyncWrapper = async () => {
+      try {
+        setIsLoading(true);
+        const data = await fetchTopRatedFilms();
+        setMovies(data);
+      } catch (error) {
+        setError(error); // Установка состояния ошибки
+        console.error("Error fetching top rated films:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
+    asyncWrapper();
+  }, []);
 
- return (
-  <div>
-    
-    <MovieList onMoviesUpdate={handleMoviesUpdate} />
+  return (
     <div>
-      <h2> Trending today</h2>
-      <ul>
+      <MovieList onMoviesUpdate={handleMoviesUpdate} />
 
-          <li>
-            <MovieList movies={movies} />
-          </li>
-            </ul>
+      {isLoading && <p>Loading...</p>}
+
+      {error && (
+        <div>
+          <p>Failed to fetch top rated films. Please try again later.</p>
+          <p>Error details: {error.message}</p>
+        </div>
+      )}
+
+      {!isLoading && !error && (
+        <div>
+          <h2>Trending today</h2>
+          <ul>
+            <li>
+              <MovieList movies={movies} />
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
 };
 
 export default HomePage;
