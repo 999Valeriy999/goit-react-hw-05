44f91b3903710b5bece836b6a406b1d57ebd7f13
Valeriy Borodenko <valeriyy999@gmail.com>
2024-07-18 22:39:02 +0300

    add

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import TrendingItem from '../TrendingItem/TrendingItem'; // Предполагается, что у вас есть компонент TrendingItem для отображения каждого элемента списка
 
-const MovieList = ({ title, movies, isLoading }) => {
+const MovieList = ({ movies }) => {
   return (
-    <div>
-      {title && <h2>{title}</h2>}
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
-          {Array.isArray(movies) && movies.map((movie) => (
-            <TrendingItem key={movie.id} id={movie.id} name={movie.title} />
-          ))}
-        </ul>
-      )}
-    </div>
+    
+      <ul>
+        {Array.isArray(movies) && movies.map((movie) => (
+          <TrendingItem key={movie.id} id={movie.id} name={movie.title} />
+        ))}
+      </ul>
+    
   );
 };
 
