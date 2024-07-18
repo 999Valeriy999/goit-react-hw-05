f26c44c18a801d7df508785081d7a091cf51e5c3
Valeriy Borodenko <valeriyy999@gmail.com>
2024-07-19 00:13:59 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,40 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import './App.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import { lazy } from "react";
-import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage'
 import Navigation from './components/Navigation/Navigation';
 
-
-const Home = lazy(() => import("./pages/HomePage/HomePage"));
-
-const MovieDetails = lazy(() =>
-  import("./pages/MovieDetailsPage/MovieDetailsPage")
-);
-const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
-
-const NotFound = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
-
-const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
-
-const MovieReviews = lazy(() =>
-  import("./components/MovieReviews/MovieReviews")
-);
-
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
+const MovieCast = lazy(() => import('./components/MovieCast/MovieCast'));
+const MovieReviews = lazy(() => import('./components/MovieReviews/MovieReviews'));
 
 function App() {
- 
   return (
-  
     <BrowserRouter>
-      <div>
-        
-       <Navigation>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink> 
-              <NavLink to ="/movies/"> Movies</NavLink>
-          
-              
-            </li>
-            
-              
-            
-          </ul>
-        </Navigation>
-        <Suspense fallback={<div>Loading...</div>}>
+      <Navigation>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies/">Movies</NavLink>
+          </li>
+        </ul>
+      </Navigation>
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path= "/movies" element = {<MovieDetails/>}/>
-          <Route path= "/MoviesPage" element = {<MoviesPage/>}/>
-          <Route path= "/cast" element = {<MoviesPage/>}/>
-
-
-
-
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
-        </Suspense>
-      </div>
-    </BrowserRouter> 
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
